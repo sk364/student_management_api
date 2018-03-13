89a76e908a64b6310bfc7f8d84b9df416b2712f1
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-03-13 20:47:22 +0530

    Added user registration api view

--- api/main/urls.py
+++ api/main/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 
 from rest_framework.routers import DefaultRouter
 
-from .views import StudentViewSet, CourseViewSet
+from .views import StudentViewSet, CourseViewSet, UserRegistrationAPIView
 
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
 router.register(r'courses', CourseViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r'register/$', UserRegistrationAPIView.as_view()),
+] + router.urls
--- api/main/views.py
+++ api/main/views.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
 
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Course
 from .permissions import IsAdmin, IsAdminOrReadOnly
 from .serializers import StudentSerializer, CourseSerializer
+
+REQUIRED_FIELDS_ABSENT_MSG = 'Required Field(s) absent.'
+MIN_PASSWORD_LENGTH_MSG = 'Password should be at least 8 characters long.'
+SUCCESS_REGISTRATION_MSG = 'Successfully Registered.'
 
 
 class StudentViewSet(ModelViewSet):
@@ -17,3 +24,52 @@ class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = (IsAdminOrReadOnly, )
+
+
+class UserRegistrationAPIView(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request):
+        """
+        API View that registers users in the app.
+
+        :form: `username` Username string
+
+        :form: `password` Password string
+
+        :response: `200` Successfully Registered
+
+        :response: `400` Required fields absent
+
+        :response: `400` Password length is less than 8
+        """
+
+        username = request.data.get('username')
+        password = request.data.get('password')
+        response = {}
+        resp_status = None
+
+        if username is None or password is None:
+            response = {
+                'success': False,
+                'message': REQUIRED_FIELDS_ABSENT_MSG
+            }
+            resp_status = status.HTTP_400_BAD_REQUEST
+        elif len(password) < 8:
+            response = {
+                'success': False,
+                'message': MIN_PASSWORD_LENGTH_MSG
+            }
+        else:
+            user = User.objects.create(username=username)
+            user.set_password(password)
+            user.save()
+
+            response = {
+                'success': True,
+                'message': SUCCESS_REGISTRATION_MSG,
+                'redirect_url': '/login'
+            }
+
+        return Response(response, status=resp_status)
