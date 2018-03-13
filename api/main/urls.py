15b347bd15b58be5d1cc14c20c3921a8894b5561
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-03-13 20:13:45 +0530

    Added views, endpoints, permissions

--- api/main/urls.py
+++ api/main/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+
 from rest_framework.routers import DefaultRouter
 
+from .views import StudentViewSet, CourseViewSet
+
+
 router = DefaultRouter()
+router.register(r'students', StudentViewSet)
+router.register(r'courses', CourseViewSet)
 
 urlpatterns = router.urls
