1fd73cef9a82f2ad1e0b290a0925770ac1ed060b
Sachin <skad5455@gmail.com>
2018-03-13 12:16:37 +0530

    Added project

---
+++ api/api/urls.py
@@ -0,0 +1,27 @@
+"""
+Student Management api URL Configuration
+"""
+
+from django.conf import settings
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='Student Management API')
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', schema_view),
+    url(r'^login/$', obtain_jwt_token),
+    url(r'^', include('main.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
