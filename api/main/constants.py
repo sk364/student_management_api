8024f29edeec68c0cce387c49282363f43e67d73
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-03-14 17:55:12 +0530

    Added functionality for auth user to enroll or leave course

---
+++ api/main/constants.py
@@ -0,0 +1,4 @@
+REQUIRED_FIELDS_ABSENT_MSG = 'Required Field(s) absent.'
+MIN_PASSWORD_LENGTH_MSG = 'Password should be at least 8 characters long.'
+SUCCESS_REGISTRATION_MSG = 'Successfully Registered.'
+MAX_STUDENTS_IN_COURSE = 5
