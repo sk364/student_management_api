a8509f48157743993ff1ed5b4d491cb2ce93cfbb
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-03-14 19:31:53 +0530

    Added change password api view

--- api/main/constants.py
+++ api/main/constants.py
@@ -1,4 +1,5 @@
 REQUIRED_FIELDS_ABSENT_MSG = 'Required Field(s) absent.'
 MIN_PASSWORD_LENGTH_MSG = 'Password should be at least 8 characters long.'
 SUCCESS_REGISTRATION_MSG = 'Successfully Registered.'
+PASSWORDS_DO_NOT_MATCH_MSG = 'Passwords do not match.'
 MAX_STUDENTS_IN_COURSE = 5
